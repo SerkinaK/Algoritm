f4091ad8de6c87b9578a34a4eb1df650f6bd1cf1
SerkinaK <serkinaksenia22@gmail.com>
2025-03-12 11:42:08 +0500

    Update GoblinsAndShamans.java

--- занятие3/GoblinsAndShamans.java
+++ занятие3/GoblinsAndShamans.java
@@ -5,60 +5,41 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class GoblinsAndShamans {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(reader.readLine());
-        Deque<Integer> queue = new ArrayDeque<>();
-        int mid = 0;
+        Deque<Integer> left = new ArrayDeque<>();
+        Deque<Integer> right = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             String[] query = reader.readLine().split(" ");
             switch (query[0]) {
                 case "+":
                     int goblinId = Integer.parseInt(query[1]);
-                    queue.addLast(goblinId);
-                    if (queue.size() % 2 != 0) {
-                        mid = (queue.size() + 1) / 2;
-                    }
+                    right.addLast(goblinId);
                     break;
                 case "*":
                     goblinId = Integer.parseInt(query[1]);
-                    if (queue.isEmpty()) {
-                        queue.addFirst(goblinId);
-                        mid = 1;
-                    } else {
-                        Deque<Integer> tempQueue = new ArrayDeque<>();
-                        int count = 0;
-                        while (count < queue.size() - mid) {
-                            tempQueue.addFirst(queue.removeLast());
-                            count++;
-                        }
-                        queue.addLast(goblinId);
-                        while (!tempQueue.isEmpty()) {
-                            queue.addLast(tempQueue.removeFirst());
-                        }
-                        if (queue.size() % 2 == 0) {
-                            mid = (queue.size() + 1) / 2;
-                        } else {
-                            mid = (queue.size() + 1) / 2;
-                        }
-                    }
+                    right.addFirst(goblinId);
                     break;
                 case "-":
-                    int removedGoblin = queue.removeFirst();
-                    writer.write(String.valueOf(removedGoblin));
+                    writer.write(String.valueOf(left.removeFirst()));
                     writer.newLine();
-                    if (queue.size() % 2 == 0) {
-                        mid = (queue.size() + 1) / 2;
-                    } else if (queue.size() > 0){
-                        mid = (queue.size() + 1) / 2;
-                    }
                     break;
             }
+            balanceQueues(left, right);
         }
         writer.flush();
         reader.close();
         writer.close();
+    }
+    private static void balanceQueues(Deque<Integer> left, Deque<Integer> right) {
+        while (left.size() > right.size() + 1) {
+            right.addFirst(left.removeLast());
+        }
+        while (right.size() > left.size()) {
+            left.addLast(right.removeFirst());
+        }
     }
 }
